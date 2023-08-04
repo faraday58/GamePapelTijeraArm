77760b058f198f81dd976bab40c6f375de177901
faraday58 <jarodriguezvera@gmail.com>
2023-08-04 15:46:50 -0600

    Initial Commit

---
+++ GamePapelTijeraArm/GamePapelTijeraArmApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamePapelTijeraArmApp.swift
+//  GamePapelTijeraArm
+//
+//  Created by alumno on 04/08/23.
+//
+
+import SwiftUI
+
+@main
+struct GamePapelTijeraArmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
